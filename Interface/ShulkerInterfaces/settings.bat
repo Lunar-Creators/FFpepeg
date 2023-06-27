a5c166b7099418bdbd23601e9f97ec8fbfbc3a76
SHULKER <92025362+SHULKERPLAY@users.noreply.github.com>
2023-06-27 10:22:13 +0000

    Editing Settings

--- Interface/ShulkerInterfaces/settings.bat
+++ Interface/ShulkerInterfaces/settings.bat
@@ -1,39 +1,48 @@
 @echo off
 if not exist settings\valid.lc goto set_reset
-
 :DisplaySettings
 rem add checks for settings. Settings are applied by variables
-rem set /p xxxx=<yyyy.lc
+rem Powershell
+if exist settings\pwshdisable.lc set set-powershell=DISABLED
+if exist settings\pwsh.lc set /p set-powershell=<settings\pwsh.lc
+if not exist settings\pwshdisable.lc if not exist settings\pwsh.lc set set-powershell=SYSTEM
+rem ffmpeg package
+if exist settings\ffmpeg.lc set set-ffmpeg=<settings\ffmpeg.lc
+if not exist settings\ffmpeg.lc set set-ffmpeg=DEFAULT
+rem explorer
+if exist settings\enablexplr.lc set set-explorer=ALWAYS
+if not exist settings\enablexplr.lc set set-explorer=NEVER
+rem Experimental Features
+if exist settings\experimental.lc set set-experimental=ENABLED
+if not exist settings\experimental.lc set set-experimental=DISABLED
+rem FFMPEG LICENSE
+if exist settings\default.lcx set set-fflicense=DEFAULT (GPL)
 cls
 echo ::::::::::::::::::::::::::
 echo Q - Powershell directory
 echo %set-powershell%
 echo W - FFmpeg package
 echo %set-ffmpeg%
-echo E - Open Explorer Window After Encoding?
+echo E - Open Output folder after Encoding?
 echo %set-explorer%
 echo O - Enable experimental features
 echo %set-experimental%
 echo P - FFmpeg license
 echo %set-fflicense%
-echo A - Apply changes
-echo X - Exit Without Applying
+echo X - Exit
 echo .
-echo H - Help
 echo M - Reset Defaults
 echo ::::::::::::::::::::::::::
-choice /C ERU /N
+choice /C QWEOPXM /N
 
 if %errorlevel%==1 goto set_powershell
 if %errorlevel%==2 goto set_FFmpeg
 if %errorlevel%==3 goto set_explorer
 if %errorlevel%==4 goto set_experimental
 if %errorlevel%==5 goto set_fflicense
-if %errorlevel%==6 goto set_apply
-if %errorlevel%==7 cd ..&run.bat
-if %errorlevel%==8 goto set_help
-if %errorlevel%==9 goto set_reset
-goto start
+if %errorlevel%==6 cd ..&run.bat
+if %errorlevel%==7 goto set_reset
+goto DisplaySettings
 
 :set_reset
 cls
@@ -41,8 +50,19 @@ echo Reset in progress...
 rd /s /q settings
 md settings
 echo Lunar Creators File>settings\valid.lc
-echo FFmpeg License. Lunar Creators>settings\default.lcx
+echo FFmpeg License. Lunar Creators File>settings\default.lcx
 echo Display explorer. Lunar Creators File>settings\enablexplr.lc
+goto DisplaySettings
+
+:set_fflicense
+cls
+echo Selected license significantly affects the list of encoders available for use according to their license
+echo ::::::::::::::::::::::::::
+echo Q - GPL (Default)
+echo E - Return
+echo ::::::::::::::::::::::::::
+echo ! We are still working on licenses. Now you can only use the default GPL license
+choice /C QE /N
 goto DisplaySettings
 
 :set_explorer
@@ -50,16 +70,42 @@ if exist settings\enablexplr.lc del /f /q settings\enablexplr.lc
 if not exist settings\enablexplr.lc echo Display explorer. Lunar Creators File>settings\enablexplr.lc
 goto DisplaySettings
 
+:set_experimental
+if exist settings\experimental.lc del /f /q settings\experimental.lc
+if not exist settings\experimental.lc echo Enable experimental features. Lunar Creators File>settings\experimental.lc
+goto DisplaySettings
+
 :set_powershell
+cls
+echo ::::::::::::::::::::::::::
+echo Q - System powershell (Default)
+echo X - Set Custom Powershell
+echo N - DISABLE POWERSHELL
+echo E - Return
+echo ::::::::::::::::::::::::::
+echo ! If you disable powershell then you will have to enter the paths to files and folders manually
+choice /C QXNE /N
+if %errorlevel%==1 del /f /q settings\pwsh.lc& del /f /q settings\pwshdisable.lc& goto DisplaySettings
+if %errorlevel%==3 echo Powershell was disabled. Lunar Creators File>settings\pwshdisable.lc& del /f /q settings\pwsh.lc& goto DisplaySettings
+if %errorlevel%==4 goto DisplaySettings
 cls
 echo Select powershell executable (pwsh.exe or powershell.exe)
 for /F "usebackq" %%a in (`powershell -executionpolicy bypass -file Get-Path.ps1 8`) do if not "%%a" == "Cancel" if not "%%a" == "OK" set decode2=%%a
 set pstemp=%decode2:?= %
 echo "%pstemp%">settings\pwsh.lc
+del /s /q settings\pwshdisable.lc
 goto DisplaySettings
 
 :set_FFmpeg
 cls
+echo ::::::::::::::::::::::::::
+echo Q - Default FFmpeg
+echo X - Set Custom FFmpeg
+echo E - Return
+echo ::::::::::::::::::::::::::
+choice /C QXE /N
+if %errorlevel%==1 del /f /q settings\ffmpeg.lc& goto DisplaySettings
+if %errorlevel%==3 goto DisplaySettings
 echo Select FFmpeg executable (ffmpeg.exe)
 for /F "usebackq" %%a in (`powershell -executionpolicy bypass -file Get-Path.ps1 8`) do if not "%%a" == "Cancel" if not "%%a" == "OK" set decode2=%%a
 set fftemp=%decode2:?= %
